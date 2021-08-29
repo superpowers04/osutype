7e7318000b2ae335037331b905b1487df78a2efb
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-08-29 11:55:29 -0400

    plugin first try

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { Plugin } = require('powercord/entities');
 
-module.exports = class Template extends Plugin {
+module.exports = class OsuTyping extends Plugin {
     startPlugin() {
-        return;
+        //add an audio element to the page root
+        const audio = document.createElement("audio");
+        audio.src = "https://github.com/happyori/OsuTyping/raw/master/menuclick.wav";
+        audio.id = "osutype";
+        audio.setAttribute("style", "display: none;");
+        document.body.appendChild(audio);
+        document.addEventListener("keydown", this.keyDown)
     }
     pluginWillUnload() {
-        return;
+        //remove the audio element from the page root
+        document.body.removeChild(document.getElementById("osutype"));
+        document.removeEventListener("keydown", this.keyDown)
     }
+    keyDown(e) {
+        //seek the audio element to 0 seconds and play it
+        document.getElementById("osutype").currentTime = 0;
+        document.getElementById("osutype").play();
+
+    }
+
 };
