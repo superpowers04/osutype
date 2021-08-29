468dd0265c5bdf75182765312cb72901615be8b9
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-08-29 12:52:57 -0400

    fix

--- component/Settings.jsx
+++ component/Settings.jsx
@@ -8,8 +8,12 @@ module.exports = class Settings extends React.PureComponent {
             minValue={1}
             maxValue={100}
             initialValue={this.props.getSetting('volume', 100)}
-            onChange={(value) => {document.getElementById('osutype').volume = (value / 100);
-            this.props.updateSetting('volume', value);}}
+            onChange={(value) => {console.log(
+                `Volume Change ${value / 100}`
+            );document.getElementById('osutype').volume = (value / 100);
+            console.log(
+                `volume: ` + document.getElementById('osutype').volume 
+            );this.props.updateSetting('volume', value);}}
         >
             Volume
         </SliderInput>
